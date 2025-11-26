72ba4eed5dbde0fccd9493d1f51804f8d5c7e631
vlad <vampir7421@gmail.com>
2025-11-26 13:27:06 +0300

    Fix login path

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -5,5 +5,11 @@ export const load: LayoutServerLoad = async (event) => {
 	if (event.locals.user) {
 		return;
 	}
+	const last = event.url.pathname.split('/').filter(Boolean).at(-1);
+
+	if (last === 'login') {
+		return;
+	}
+
 	return redirect(308, '/');
 };
